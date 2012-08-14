4e3c7ab7c42cbfd339fee1d4c3f7c1efe818b757
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-08-14 14:52:26 +0100

    Publish 'Live at' as a part of the API
    Author: @pbadenski

--- lib/app.rb
+++ lib/app.rb
@@ -13,12 +13,20 @@ configure do
   end
 end
 
+def most_recent_collection_date(visitors)
+  visitors.map(&:collected_at).max
+end
+
 get '/todays-activity' do
   content_type :json
-  today     = Hash[TodaysActivity.visitors_today.map {|each| [each.start_at.hour, each.value] }]
-  yesterday = Hash[TodaysActivity.visitors_yesterday.map {|each| [each.start_at.hour, each.value]}]
-  average   = Hash[TodaysActivity.last_month_average.map {|each| [each[:hour], each[:value]]}]
-  todays_activity = (0..23).map { |hour|
+  visitors_today = TodaysActivity.visitors_today
+  visitors_yesterday = TodaysActivity.visitors_yesterday
+  last_month_average = TodaysActivity.last_month_average
+  today     = Hash[visitors_today.map {|each| [each.start_at.hour, each.value] }]
+  yesterday = Hash[visitors_yesterday.map {|each| [each.start_at.hour, each.value]}]
+  average   = Hash[last_month_average.map {|each| [each[:hour], each[:value]]}]
+  todays_activity = {}
+  todays_activity[:values] = (0..23).map { |hour|
     result = {
       :hour_of_day => hour,
       :visitors => {
@@ -32,5 +40,7 @@ get '/todays-activity' do
     result
   }
 
+  todays_activity[:live_at] = 
+    most_recent_collection_date(visitors_today + visitors_yesterday)
   todays_activity.to_json
 end

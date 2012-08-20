ebdfd9149f65548323ab62f5a2dea1a89d4cc6b0
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-08-20 12:13:53 +0100

    Validate length of hour.
    The start at and end at period should span excaply one hour.
    
    author: @robyoung
    author: @jthoenes

--- lib/unique_visitors_model.rb
+++ lib/unique_visitors_model.rb
@@ -15,4 +15,33 @@ class UniqueVisitors
   property :start_at, DateTime
   property :end_at, DateTime
   property :value, Integer
+
+  validates_with_method :validate_hour_period
+  validates_with_method :validate_start_at_full_hour
+  validates_with_method :validate_end_at_full_hour
+
+  private
+  def validate_hour_period
+    if (end_at - start_at) == Rational(1, 24)
+      true
+    else
+      [false, "The time between start at and end at should be an hour."]
+    end
+  end
+
+  def validate_start_at_full_hour
+    validate_full_hour(start_at, "start at")
+  end
+
+  def validate_end_at_full_hour
+    validate_full_hour(end_at, "end at")
+  end
+
+  def validate_full_hour(time, name)
+    if time.minute == 0 and time.second == 0 and time.second_fraction == 0
+      true
+    else
+      [false, "The #{name} time has to be at a full hour."]
+    end
+  end
 end
--- spec/unit/todays_activity_spec.rb
+++ spec/unit/todays_activity_spec.rb
@@ -146,4 +146,38 @@ describe "TodaysActivity" do
     end
 
   end
+
+  describe "hour length validation" do
+    it "should be valid if period is one hour" do
+      unique_visitors = FactoryGirl.build(:unique_visitors,
+                        :start_at => DateTime.parse("2012-08-19T01:00:00+00:00"),
+                        :end_at => DateTime.parse("2012-08-19T02:00:00+00:00"))
+
+      unique_visitors.should be_valid
+    end
+
+    it "should not be valid if period is bigger than one hour" do
+      unique_visitors = FactoryGirl.build(:unique_visitors,
+                                          :start_at => DateTime.parse("2012-08-19T01:00:00+00:00"),
+                                          :end_at => DateTime.parse("2012-08-19T02:12:00+00:00"))
+
+      unique_visitors.should_not be_valid
+    end
+
+    it "should not be valid if period is smaller than one hour" do
+      unique_visitors = FactoryGirl.build(:unique_visitors,
+                                          :start_at => DateTime.parse("2012-08-19T01:00:00+00:00"),
+                                          :end_at => DateTime.parse("2012-08-19T01:59:00+00:00"))
+
+      unique_visitors.should_not be_valid
+    end
+
+    it "should always be referencing a full hour" do
+      unique_visitors = FactoryGirl.build(:unique_visitors,
+                                          :start_at => DateTime.parse("2012-08-19T01:01:00+00:00"),
+                                          :end_at => DateTime.parse("2012-08-19T02:01:00+00:00"))
+
+      unique_visitors.should_not be_valid
+    end
+  end
 end

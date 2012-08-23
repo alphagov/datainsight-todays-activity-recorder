981b782f4fd15fb2daa76342ab804fa4e5b37b7a
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-08-23 16:19:59 +0100

    Remove older releases on deploment in release.sh.
    - Added clean_old_files() method to release.sh
    - Introduced PROJECT_NAME variable in release.sh
    
    author: @jthoenes

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ set -e
 ANSI_YELLOW="\033[33m"
 ANSI_RED="\033[31m"
 ANSI_RESET="\033[0m"
+PROJECT_NAME=datainsight-todays-activity-recorder
 
 export VERSION="$1"
 if [ -z "$VERSION" ]; then
@@ -19,15 +20,24 @@ fi
 #HOST="deploy@datainsight"
 HOST="deploy@datainsight.alphagov.co.uk"
 
-scp datainsight-todays-activity-recorder-$VERSION.zip $HOST:/srv/datainsight-todays-activity-recorder/packages
+# remove older deployments
+clean_old_files() {
+    DIR=$1
+    OLD=$(ssh $HOST "ls -t '$DIR'" | tail -n +2)
+    for file in $OLD; do echo "remove $DIR/$file"; ssh $HOST rm -rf $DIR/$file; done
+}
+clean_old_files /srv/$PROJECT_NAME/release
+clean_old_files /srv/$PROJECT_NAME/packages
+
+scp $PROJECT_NAME-$VERSION.zip $HOST:/srv/$PROJECT_NAME/packages
 # deploy
 echo -e "${ANSI_YELLOW}Deploying package${ANSI_RESET}"
-ssh $HOST "mkdir /srv/datainsight-todays-activity-recorder/release/$VERSION; unzip -o /srv/datainsight-todays-activity-recorder/packages/datainsight-todays-activity-recorder-$VERSION.zip -d /srv/datainsight-todays-activity-recorder/release/$VERSION;"
+ssh $HOST "mkdir /srv/$PROJECT_NAME/release/$VERSION; unzip -o /srv/$PROJECT_NAME/packages/$PROJECT_NAME-$VERSION.zip -d /srv/$PROJECT_NAME/release/$VERSION;"
 # link
 echo -e "${ANSI_YELLOW}Linking package${ANSI_RESET}"
-ssh $HOST "rm /srv/datainsight-todays-activity-recorder/current; ln -s /srv/datainsight-todays-activity-recorder/release/$VERSION/ /srv/datainsight-todays-activity-recorder/current;"
+ssh $HOST "rm /srv/$PROJECT_NAME/current; ln -s /srv/$PROJECT_NAME/release/$VERSION/ /srv/$PROJECT_NAME/current;"
 # restart
 echo -e "${ANSI_YELLOW}Restarting web service${ANSI_RESET}"
-ssh $HOST "sudo service datainsight-todays-activity-recorder-web restart"
+ssh $HOST "sudo service $PROJECT_NAME-web restart"
 echo -e "${ANSI_YELLOW}Restarting listener${ANSI_RESET}"
-ssh $HOST "sudo service datainsight-todays-activity-recorder-listener restart"
+ssh $HOST "sudo service $PROJECT_NAME-listener restart"

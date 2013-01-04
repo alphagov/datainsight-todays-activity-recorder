95db0e635ae0460773608d5d0815609f1c7809d4
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-01-04 11:22:14 +0000

    Perform migration only if required.
    This addresses issues with running both auto-upgrade and explicit
    migrations.
    
    @pbadenski
    @robyoung

--- db/migrate/use_recorder_gem_for_daily_visitors.rb
+++ db/migrate/use_recorder_gem_for_daily_visitors.rb
@@ -4,9 +4,17 @@ require "dm-migrations/migration_runner"
 migration 1, :use_recorder_gem_for_daily_visitors do
   up do
     modify_table :daily_unique_visitors do
-      drop_column :updated_at
-      drop_column :created_at
-      add_column :source, String, allow_nil: false
+      if adapter.field_exists?("daily_unique_visitors", "updated_at")
+        drop_column :updated_at
+      end
+
+      if adapter.field_exists?("daily_unique_visitors", "created_at")
+        drop_column :created_at
+      end
+
+      unless adapter.field_exists?("daily_unique_visitors", "source")
+         add_column :source, String, allow_nil: false
+      end 
     end
   end
 end
--- db/migrate/use_recorder_gem_for_hourly_visitors.rb
+++ db/migrate/use_recorder_gem_for_hourly_visitors.rb
@@ -1,12 +1,23 @@
 require "data_mapper"
 require "dm-migrations/migration_runner"
 
+
+
 migration 2, :use_recorder_gem_for_hourly_visitors do
   up do
     modify_table :hourly_unique_visitors do
-      drop_column :updated_at
-      drop_column :created_at
-      add_column :source, String, allow_nil: false
+      if adapter.field_exists?("hourly_unique_visitors", "updated_at")
+        drop_column :updated_at
+      end
+
+      if adapter.field_exists?("hourly_unique_visitors", "created_at")
+        drop_column :created_at
+      end
+
+      
+      unless adapter.field_exists?("hourly_unique_visitors", "source")
+        add_column :source, String, allow_nil: false
+      end
     end
   end
 end
